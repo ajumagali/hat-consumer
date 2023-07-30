a342a838f47e4ac9ce8dc364e4896ec665966cf9
Asyl Jumagaliyev <ajumagal@jaguarlandrover.com>
2023-07-30 13:01:03 +0100

    initial commit

---
+++ src/test/java/com/ajumagali/consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajumagali.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
